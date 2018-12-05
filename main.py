54fb9b2233f574349b4a3f2c65014b6449d9d054
Rodrigo Santos <rodrigobsi@usp.br>
2018-12-05 10:12:14 -0200

    Pegando dados do arquivo.Tratando os dados.Colocando em RAM em uma matriz

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ lista_linhas=[]
 matriz_cidades =[]
 arq = open('brazil58.tsp', 'r')
 lista_linhas = arq.readlines()
-tamanho_matriz= lista_linhas[3][11:len(lista_linhas[3])]
-print (tamanho_matriz)
+tamanho_matriz= int(lista_linhas[3][11:len(lista_linhas[3])])
+
+for i in range(tamanho_matriz-1):
+    temp =[]
+    linha_aux = lista_linhas[7+i].split(" ")
+    for j in range(len(linha_aux)):
+        elemento = linha_aux[j]
+        temp.append(elemento)
+    matriz_cidades.append(temp[:])
+
+print (matriz_cidades)
 arq.close()
