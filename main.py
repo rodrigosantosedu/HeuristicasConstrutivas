9a08d511a54cd1602d0aa32a64289bb73b796659
Rodrigo Santos <rodrigobsi@usp.br>
2018-12-05 10:20:37 -0200

    inserção na matriz(int)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for i in range(tamanho_matriz-1):
     temp =[]
     linha_aux = lista_linhas[7+i].split(" ")
     for j in range(len(linha_aux)):
-        elemento = linha_aux[j]
+        elemento = int(linha_aux[j])
         temp.append(elemento)
     matriz_cidades.append(temp[:])
 # FIM: Pegar dados do arquivo e organizar na matriz
