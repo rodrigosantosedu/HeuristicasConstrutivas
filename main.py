f23ee6d61df34ad924df5fe95fa84fe76762cb22
Rodrigo Santos <rodrigobsi@usp.br>
2018-12-05 10:16:21 -0200

    Pegando dados do arquivo.Tratando os dados.Colocando em RAM em uma matriz.Comentários atualizados

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ lista_linhas=[]
 matriz_cidades =[]
 arq = open('brazil58.tsp', 'r')
 lista_linhas = arq.readlines()
-tamanho_matriz= int(lista_linhas[3][11:len(lista_linhas[3])])
+tamanho_matriz= int(lista_linhas[3][11:len(lista_linhas[3])]) # identificando número de cidades disponibilizados no arquivo
 
+# INICIO: Pegar dados do arquivo e organizar na matriz
 for i in range(tamanho_matriz-1):
     temp =[]
     linha_aux = lista_linhas[7+i].split(" ")
@@ -12,6 +13,7 @@ for i in range(tamanho_matriz-1):
         elemento = linha_aux[j]
         temp.append(elemento)
     matriz_cidades.append(temp[:])
+# FIM: Pegar dados do arquivo e organizar na matriz
 
 print (matriz_cidades)
 arq.close()
